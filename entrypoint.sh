0997a4291d100160143351e853e8b85e386d807b
Christopher Chow <cchow@scentregroup.com>
2018-04-11 09:27:33 +1000

    fix(docker): remove echo statements in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,6 @@ set -e
 if [ "${1}" = "nginx" ]; then
   sed -i -e "s/#CACHE_TTL#/${CACHE_TTL}/g" /etc/nginx/conf.d/default.conf
   sed -i -e "s/#HOST_TO_CACHE#/${HOST_TO_CACHE}/g" /etc/nginx/conf.d/default.conf
-
-  echo "Caching: ${HOST_TO_CACHE}"
-  echo "Cache TTL: ${REST_HOST}"
-  echo "Starting nginx..."
 fi
+
 exec "$@"
