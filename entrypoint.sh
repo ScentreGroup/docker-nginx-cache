a23b75aae642993ab9aa476d3973c3f82325b53d
Christopher Chow <cchow@scentregroup.com>
2018-04-11 12:28:51 +1000

    fix: lower the lock timeout to 1 second

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ "${1}" = "nginx" ]; then
   sed -i -e "s/#CACHE_TTL#/${CACHE_TTL}/g" /etc/nginx/conf.d/default.conf
+  sed -i -e "s/#CACHE_LOCK_TIMEOUT#/${CACHE_LOCK_TIMEOUT}/g" /etc/nginx/conf.d/default.conf
   sed -i -e "s/#HOST_TO_CACHE#/${HOST_TO_CACHE}/g" /etc/nginx/conf.d/default.conf
 fi
 
